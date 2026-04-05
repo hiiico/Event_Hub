b3005b335196af7e49b887cca0bc5f6701812afb
hiiico <hiiico332@gmail.com>
2026-04-05 11:05:12 +0300

    setup forms validation and buttons feedback

--- frontend/src/app/core/interceptors/auth.interceptor.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -5,12 +5,11 @@ import { AuthService } from '../services/auth/auth.service';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
-  console.log('Interceptor – token:', token);
+
   if (token) {
     const cloned = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
-    console.log('Interceptor – added Authorization header');
     return next(cloned);
   }
   return next(req);
