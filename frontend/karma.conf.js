b6a4d63ac6731dd4676f00ccc32b5f32a31ad362
hiiico <hiiico332@gmail.com>
2026-04-08 15:48:24 +0300

    setup jasmine und karma

---
+++ frontend/karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('karma-jasmine-html-reporter')
+    ],
+    client: {
+      clearContext: false
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
